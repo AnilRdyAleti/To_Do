a13720b10d98b23571d4cffa49db6e496030204e
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-29 17:20:25 -0400

    Added base fragment and added libraries in gradle files

--- app/src/main/java/com/example/anilreddy/to_do_project/MainActivity.java
+++ app/src/main/java/com/example/anilreddy/to_do_project/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
+import com.example.anilreddy.to_do_project.appDefault.BaseActivity;
+
 public class MainActivity extends BaseActivity {
 
     @Override
--- app/src/main/java/com/example/anilreddy/to_do_project/appDefault/BaseActivity.java
+++ app/src/main/java/com/example/anilreddy/to_do_project/appDefault/BaseActivity.java
@@ -1,4 +1,4 @@
-package com.example.anilreddy.to_do_project;
+package com.example.anilreddy.to_do_project.appDefault;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.anilreddy.to_do_project.R;
 
 public abstract class BaseActivity extends AppCompatActivity {
 

7a76a3878dd897c610439531145d49f5afa75892
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-29 17:06:49 -0400

    Added base activity and every activity should extend base activity

---
+++ app/src/main/java/com/example/anilreddy/to_do_project/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.example.anilreddy.to_do_project;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(contentViewLayoutRes());
+        setUpInitialFragment(savedInstanceState);
+    }
+
+    private void setUpInitialFragment(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,createInitialFragment())
+                    .commit();
+        }
+    }
+
+    @NonNull
+    protected abstract Fragment createInitialFragment();
+
+    @LayoutRes
+    protected abstract int contentViewLayoutRes();
+
+}
--- app/src/main/java/com/example/anilreddy/to_do_project/MainActivity.java
+++ app/src/main/java/com/example/anilreddy/to_do_project/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.anilreddy.to_do_project;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @NonNull
+    @Override
+    protected Fragment createInitialFragment() {
+        return null;
+    }
+
+    @Override
+    protected int contentViewLayoutRes() {
+        return R.layout.activity_main;
     }
 }
